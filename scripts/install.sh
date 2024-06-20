e7feeab63a05539035d5fc7785b1176b38adf41d
Amal Thundiyil <mail@amalthundiyil.com>
2024-06-20 14:32:08 +0200

    fix install script
    Signed-off-by: Amal Thundiyil <mail@amalthundiyil.com>

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,7 +2,7 @@
 
 set -xe
 
-sudo apt install git make -y
+sudo apt install git make fuse -y
 
 PROJECT_DIR="/home/ubuntu/ssbench"
 # PROJECT_DIR="/home/amal/work/ssbench"
@@ -35,7 +35,7 @@ sudo tar -C $PROJECT_DIR/bin -xvf /tmp/soci-snapshotter-0.6.1-linux-amd64.tar.gz
 wget https://raw.githubusercontent.com/awslabs/soci-snapshotter/main/soci-snapshotter.service -O /usr/local/lib/systemd/system/soci-snapshotter.service
 
 # stargz
-if [! -f /tmp/stargz-snapshotter-v0.15.1-linux-amd64.tar.gz ]; then
+if [ ! -f /tmp/stargz-snapshotter-v0.15.1-linux-amd64.tar.gz ]; then
     wget https://github.com/containerd/stargz-snapshotter/releases/download/v0.15.1/stargz-snapshotter-v0.15.1-linux-amd64.tar.gz -P /tmp
 fi
 sudo tar -C /usr/local/bin -xvf /tmp/stargz-snapshotter-v0.15.1-linux-amd64.tar.gz
@@ -47,8 +47,10 @@ if [ ! -d /tmp/cvmfs ]; then
 fi
 cd /tmp/cvmfs/snapshotter
 make
-mv /tmp/cvmfs/snapshotter/out/cvmfs_snapshotter $PROJECT_DIR/bin/cvmfs_snapshotter
+cp /tmp/cvmfs/snapshotter/out/cvmfs_snapshotter $PROJECT_DIR/bin/cvmfs_snapshotter
+cp /tmp/cvmfs/snapshotter/out/cvmfs_snapshotter /usr/local/bin/cvmfs_snapshotter
 wget https://raw.githubusercontent.com/cvmfs/cvmfs/42e04529dc8eccb52bf62b27b220aa54b660681a/snapshotter/script/config/etc/systemd/system/cvmfs-snapshotter.service -O /usr/local/lib/systemd/system/cvmfs-snapshotter.service
+mkdir -p /etc/containerd-cvmfs-grpc && touch /etc/containerd-cvmfs-grpc/config.toml
 
 
 cd $PROJECT_DIR 
